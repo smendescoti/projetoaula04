f55e0ae388d32301e44e1a4334706fed6d40ec41
smendescoti <sergio.coti@gmail.com>
2023-02-11 12:28:18 -0300

    first commit

---
+++ ProjetoAula04/Interfaces/IController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula04.Interfaces
+{
+    public interface IController
+    {
+        void Cadastrar();
+        void Atualizar();
+        void Excluir();
+        void Consultar();
+    }
+}
